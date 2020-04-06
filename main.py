ce388c9ff50a3e9d6aae38ead735767882b77631
Anthon98 <wesobix@gmail.com>
2020-04-06 05:57:15 +0300

    commands added

--- main.py
+++ main.py
@@ -1,25 +1,40 @@
 import discord
 from discord.ext import commands
 
+import time
+
 client = commands.Bot(command_prefix = '--')
 
 @client.event
 async def on_ready():
-    print("online")
+    
+    t = time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
+    print("Activated. @ -> "+ str(current_time))
+    
 
 @client.event
 async def on_member_join(m):
     print(str(m)+" joined.")
-    #chaid = '692025511727333407'
     channel = client.get_channel(696503641309446144)
-
     await channel.send(str(m)+' joined.')
 
 @client.event
 async def on_member_remove(m):
     print(str(m)+" left.")
-    #chaid = '692025511727333407'
     channel = client.get_channel(696503641309446144)
     await channel.send(str(m)+' left.')
+
+@client.command()
+async def on_message(msg):
+    if msg.author == client.user:
+        return
+    if msg.author.bot:
+        return
+    if msg.content.startswith('--'):
+        message = msg[2:]
+        if message == "latency":
+            channel = client.get_channel(696503641309446144)
+            await channel.send(f'Latency: {round(client.latency * 1000)}ms')
 
 client.run('Njk2NTAwODI4MzU3NTI1NjM0.XoppCw.xi99nJizoTtnnkq-kJikM7BKwDE')
